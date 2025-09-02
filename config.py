5b013af77c8365a715cec35ffc60bb1d66ff0d8e
unknown <jumak4863@gmail.com>
2025-09-02 07:19:34 +0500

    Configure for Heroku database

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'something to change later')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', 'sqlite:///school.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///school.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
